42815afa811e0a06909ed3a4c8321013d6d2b9a9
Mario.Ametrano <wario.ametrano@hotmail.com>
2026-03-06 11:46:24 +0100

    update test_formy

--- UI Tests/Pytest/test_formy.py
+++ UI Tests/Pytest/test_formy.py
@@ -42,29 +42,38 @@ def test_form(driver):
 
     # Street address
     driver.find_element(By.ID, "street_number").send_keys("Via Roma 1")
-    
     # Street address 2 (Corretto il selettore che era rotto)
     driver.find_element(By.ID, "route").send_keys("Via Fasulla 123")
-    
     # City
     driver.find_element(By.ID, "locality").send_keys("Verona")
-    
     # State
     driver.find_element(By.ID, "administrative_area_level_1").send_keys("VR")
-    
     # Zip code
     driver.find_element(By.ID, "postal_code").send_keys("37121")
-    
     # Country
     driver.find_element(By.ID, "country").send_keys("Italy")
 
     sleep(2) 
     driver.back()
     sleep(2) 
-    
+
     # Verifica che siamo tornati indietro (opzionale)
     wait.until(EC.presence_of_element_located((By.LINK_TEXT, "Autocomplete")))
     print("Test completato con successo!")
+
+def test_datepicker(driver):
+    wait = WebDriverWait(driver, 10)
+    
+    driver.get("https://formy-project.herokuapp.com/datepicker")
+
+    wait.until(EC.element_to_be_clickable((By.ID, "datepicker"))).click()
+
+
+    sleep(5) 
+
+    print("Test completato con successo!")
+   #PER ESEGUIRE UN SINGOLOTEST: pytest "UI Tests\Pytest\test_formy.py::test_datepicker"
+
 
 # Nota: Se usi Pytest, l'input() finale non serve perché Pytest gestisce la chiusura.
 # Se vuoi vederlo fermo, lascia lo sleep più lungo.
